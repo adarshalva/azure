7d363df521e1be32147988206fa94e7ca14975b6
AbhishekLK20 <153344921+AbhishekLK20@users.noreply.github.com>
2025-06-27 23:10:07 +0530

    Update flight.ts

--- server/routers/flight.ts
+++ server/routers/flight.ts
@@ -1,22 +1,22 @@
-import { Router } from "express";
-
-import {getFlights} from "../controllers/flight";
-
-const router: Router = Router();
-
-router.get("/", getFlights);
-
-export default router;
-
 // import { Router } from "express";
-// import { getFlights, createFlight } from "../controllers/flight";
+
+// import {getFlights} from "../controllers/flight";
 
 // const router: Router = Router();
 
-// // To fetch flights (either from external API or DB)
 // router.get("/", getFlights);
 
-// // To create a new flight and store it in your database
-// router.post("/", createFlight);
-
 // export default router;
+
+import { Router } from "express";
+import { getFlights, createFlight } from "../controllers/flight";
+
+const router: Router = Router();
+
+// To fetch flights (either from external API or DB)
+router.get("/", getFlights);
+
+// To create a new flight and store it in your database
+router.post("/", createFlight);
+
+export default router;
