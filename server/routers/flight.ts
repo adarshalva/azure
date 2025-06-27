d2a2f20a8d35a0e2316833a691c17d7932231459
AbhishekLK20 <153344921+AbhishekLK20@users.noreply.github.com>
2025-06-27 21:56:18 +0530

    Update flight.ts

--- server/routers/flight.ts
+++ server/routers/flight.ts
@@ -1,10 +1,19 @@
-import { Router } from "express";
+// import { Router } from "express";
 
-import {getFlights} from "../controllers/flight";
+// import {getFlights} from "../controllers/flight";
+
+// const router: Router = Router();
+
+// router.get("/", getFlights);
+
+// export default router;
+
+import { Router } from "express";
+import { getFlights, createFlight } from "../controllers/flight";
 
 const router: Router = Router();
 
 router.get("/", getFlights);
-
+router.post("/create", createFlight);
 
 export default router;
