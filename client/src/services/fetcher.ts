736cb498c929a5c5200ca08afd170b57ea69469f
AbhishekLK20 <153344921+AbhishekLK20@users.noreply.github.com>
2025-06-27 20:17:04 +0530

    fetcher.ts

--- client/src/services/fetcher.ts
+++ client/src/services/fetcher.ts
@@ -1,17 +1,35 @@
+// import axios from 'axios';
+
+// const axiosInstance = axios.create({
+//     baseURL:( process.env.REACT_APP_BASE_URL || 'http://localhost:5000' )+ '/api',
+// });
+
+// axiosInstance.interceptors.response.use(
+//     (res) => {
+//         return res;
+//     },
+//     (error) =>Promise.reject((error.response && error.response.data) || 'Something went wrong')
+// );
+
+// export default axiosInstance;
+
+
+// export const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
+
+
 import axios from 'axios';
 
+// Only use the deployed backend URL
 const axiosInstance = axios.create({
-    baseURL:( process.env.REACT_APP_BASE_URL || 'http://localhost:5000' )+ '/api',
+  baseURL: `${process.env.REACT_APP_BACKEND_URL}/api`,
 });
 
 axiosInstance.interceptors.response.use(
-    (res) => {
-        return res;
-    },
-    (error) =>Promise.reject((error.response && error.response.data) || 'Something went wrong')
+  (res) => res,
+  (error) =>
+    Promise.reject((error.response && error.response.data) || 'Something went wrong')
 );
 
 export default axiosInstance;
-
 
 export const fetcher = (url: string) => axiosInstance.get(url).then((res) => res.data);
