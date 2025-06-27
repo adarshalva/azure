7744549de8197b021e5870ffb7e2020d877a0d40
AbhishekLK20 <153344921+AbhishekLK20@users.noreply.github.com>
2025-06-27 22:21:10 +0530

    Update flight.ts

--- server/routers/flight.ts
+++ server/routers/flight.ts
@@ -13,7 +13,10 @@ import { getFlights, createFlight } from "../controllers/flight";
 
 const router: Router = Router();
 
+// To fetch flights (either from external API or DB)
 router.get("/", getFlights);
-router.post("/create", createFlight);
+
+// To create a new flight and store it in your database
+router.post("/", createFlight);
 
 export default router;
