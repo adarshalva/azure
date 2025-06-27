200381f89bdf17a6d382b2f5fe73cc166a29abff
AbhishekLK20 <153344921+AbhishekLK20@users.noreply.github.com>
2025-06-27 23:09:31 +0530

    Update flight.ts

--- server/models/flight.ts
+++ server/models/flight.ts
@@ -1,21 +1,21 @@
-// import mongoose, { Document, Schema } from "mongoose";
+import mongoose, { Document, Schema } from "mongoose";
 
-// export interface IFlight extends Document {
-//   airline: string;
-//   route: string;
-//   scheduleDate: Date;
-//   departureTime?: Date;
-//   arrivalTime?: Date;
-//   // Add other fields as needed (e.g., origin, destination, price)
-// }
+export interface IFlight extends Document {
+  airline: string;
+  route: string;
+  scheduleDate: Date;
+  departureTime?: Date;
+  arrivalTime?: Date;
+  // Add other fields as needed (e.g., origin, destination, price)
+}
 
-// const FlightSchema: Schema = new Schema({
-//   airline: { type: String, required: true },
-//   route: { type: String, required: false },
-//   scheduleDate: { type: Date, required: true },
-//   departureTime: { type: Date },
-//   arrivalTime: { type: Date },
-//   // Additional fields can be defined here
-// });
+const FlightSchema: Schema = new Schema({
+  airline: { type: String, required: true },
+  route: { type: String, required: false },
+  scheduleDate: { type: Date, required: true },
+  departureTime: { type: Date },
+  arrivalTime: { type: Date },
+  // Additional fields can be defined here
+});
 
-// export default mongoose.model<IFlight>("Flight", FlightSchema);
+export default mongoose.model<IFlight>("Flight", FlightSchema);
